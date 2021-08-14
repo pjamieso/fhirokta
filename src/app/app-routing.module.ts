e22e4f9768e9e8babeedda2ef5a9848e95e83035
Patrick Jamieson <pjamieson@logicalsemantics.com>
2021-08-14 16:46:50 -0400

    Added OktaAuth Guards to Patient and Observation Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ObservationsComponent } from './components/observations/observations.component';
 import { PatientsComponent } from './components/patients/patients.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   { path: 'login/callback', component: OktaCallbackComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'patients', component: PatientsComponent },
-  { path: 'observations/:id', component: ObservationsComponent },
+  { path: 'patients', component: PatientsComponent, canActivate: [OktaAuthGuard] },
+  { path: 'observations/:id', component: ObservationsComponent, canActivate: [OktaAuthGuard] },
   { path: '', redirectTo: '/patients', pathMatch: 'full' },
   { path: '**', redirectTo: '/patients', pathMatch: 'full' }
 ];
